b957def5cfd1020fe74f9d18f6c3449bc3249a30
Dylan Gould <dylangood@gmail.com>
2017-12-23 13:29:29 -0600

    Pass all tests for all queries in subsection 5 of Arcade mode Databases section.

---
+++ arcade/databases/countriesInfo.sql
@@ -0,0 +1,6 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE countriesInfo()
+BEGIN
+	SELECT COUNT(name) AS number, AVG(population) as average, SUM(population) AS total
+    FROM countries;
+END
